9dbee9a9fa26ee67062f8dddde2874b42a8a6387
mauropahoor <mauro.pahoor@gmail.com>
2022-06-05 19:37:58 -0300

    .

--- Prova01/prova01.c
+++ Prova01/prova01.c
@@ -6,6 +6,7 @@ int precoAgua = 1;
 int saldo = 20;
 
 void comprar(int option){
+    /* muda o saldo dependendo do produto escolhido */
     if(option == 1 && saldo >= precoPao){
         saldo -= precoPao;
     }
@@ -20,16 +21,20 @@ void comprar(int option){
     }
 }
 void adicionarSaldo(int quant){
+    /* adiciona ao saldo a opcao desejada */
     saldo += quant;
 }
 
 int main(){
     int option;
     int quant;
+    /* cria um loop infinito*/
     while (1 > 0)
     {
+        /* pergunta a opcao deseja e a guarda em uma variavel */
         printf("Bem vindo a minha loja!\nVoce possui %d de saldo.\nDigite o que deseja comprar:\n\n1:Pao - R$ 2\n2:Agua - R$ 1\n3:Suco - R$ 4\n4: Adicionar saldo\n", saldo);
         scanf("%d", &option);
+        /* chama a funcao dependendo do numero da opcao */
         if(option == 4){
             printf("Deseja adionar quanto ao seu saldo?\n");
             scanf("%d", &quant);
--- Prova01/prova02.c
+++ Prova01/prova02.c
@@ -2,10 +2,12 @@
 
 int main(){
 
-    int valor = 0;
+    int valor;
 
+    /* pega o valor de um numero */
     printf("Digite um valor para checar se ele e positivo ou negativo.\n");
     scanf("%d", &valor);
+    /* checa se ele e positivo, negativo ou nulo */
     if(valor > 0){
         printf("O valor digitado e positivo.");
     }
@@ -13,6 +15,6 @@ int main(){
         printf("O valor digitado e negativo.");
     }
     else{
-        printf("O valor digitado e nulo.");
+        printf("O valor digitado e nulo.");    
     }
 }
